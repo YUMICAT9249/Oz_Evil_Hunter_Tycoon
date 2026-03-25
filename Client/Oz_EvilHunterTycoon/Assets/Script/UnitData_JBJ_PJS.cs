d74144cfd4d6c1314aa3201a38a1a45c03be0c4e
ATUM91 <darkholic290@gmail.com>
2026-03-25 15:53:44 +0900

    - feat UnitData_JBJ_PJS 스크립트 헌터 데이터 추가 - refactor AreType_PJS 스크립트 해당 스크립트 삭제 HunterManager_PJS 스크립트로 로직 이동

--- Client/Oz_EvilHunterTycoon/Assets/Script/UnitData_JBJ_PJS.cs
+++ Client/Oz_EvilHunterTycoon/Assets/Script/UnitData_JBJ_PJS.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
+public enum Unit
+{ 
+    NONE,
+    Hunter,
+    Monster
+}
+
 [CreateAssetMenu(menuName = "Unit Data")]
 public class UnitData_JBJ_PJS : ScriptableObject
 {
-    [Header("헌터 상세 스텟")]
-    // 헌터 스텟 채우는 부분
-
+    [Header("유닛 상세 스텟(공통)")]
+    public float maxHp = 100f;              // 최대 체력
+    public float attackDamage = 10f;        // 공격력
+    public float attackCooldown = 1.5f;     // 공격 쿨타임
+    public float moveSpeed = 0.5f;          // 이동 속도
+    public float detectRange = 5f;          // 감지 범위
+    public float attackRange = 1.2f;        // 공격 범위
+    
     [Header("몬스터 상세 스텟")]
-    public float maxHp = 100f;              // 몬스터 체력
-    public float attackDamage = 10f;        // 몬스터 공격력
-    public float moveSpeed = 0.5f;          // 몬스터 이동 속도
-    public float detectRange = 5f;          // 몬스터 감지 범위
-    public float attackRange = 1.2f;        // 몬스터 공격 범위
-    public float attackCooldown = 1.5f;     // 몬스터 공격 쿨타임
+    // 몬스터 상세 스텟
+
+    [Header("헌터 상세 스텟")]
+    public string hunterName;              // 헌터 이름
+    public HunterJop hunterJop;            // 헌터 직업
+    public int maxLevel = 100;              // 최대 레벨
+
+    public float defence = 10;           // 기본 방어력
+    public float criticalChance = 10.0f; // 기본 치명타확률
+    public float dodgeChance = 10.0f;    // 기본 회피확률
 }
