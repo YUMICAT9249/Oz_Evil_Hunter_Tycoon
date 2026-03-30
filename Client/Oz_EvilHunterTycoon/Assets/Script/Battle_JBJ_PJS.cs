cfda9240b461357d9d270e64d208394a8b96b6ca
ATUM91 <darkholic290@gmail.com>
2026-03-30 17:04:01 +0900

    refactor 전투 로직 통합(수정중)

---
+++ Client/Oz_EvilHunterTycoon/Assets/Script/Battle_JBJ_PJS.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System;
+
+// 헌터 몬스터 통합 전투 로직
+
+public class Battle_JBJ_PJS : MonoBehaviour
+{
+    private HunterData_PJS _hunterData;
+    private HunterController_PJS _hunterController;
+    // 몬스터 ~~ 
+
+    // 누가, 누굴, float 만큼의 데미지로 때렸나
+    public static Action<GameObject, GameObject, float> UnitAttack;
+
+    void Awake()
+    {
+        _hunterData = GetComponent<HunterData_PJS>();
+        _hunterController = GetComponent<HunterController_PJS>();
+        // 몬스터 ~~
+
+        if (_hunterData == null)
+        {
+            Debug.LogError("HunterData 연결 안됨", gameObject);
+        }
+        if (_hunterController == null)
+        {
+            Debug.LogError("HunterController 연결 안됨", gameObject);
+        }
+
+        // 몬스터 ~~
+    }
+    
+    public void TakeDamage()
+    { 
+    
+    }
+
+    public void GiveDamage()
+    { 
+    
+    }
+
+    // 구독
+    void OnEnable()
+    {
+        UnitAttack += AttackEvent;
+    }
+
+    // 구독 해제
+    void OnDisable()
+    {
+        UnitAttack -= AttackEvent;
+    }
+
+    private void AttackEvent(GameObject attacker, GameObject target, float damage)
+    {
+
+    }
+
+    private void FinalDamage(float damage, GameObject attacker)
+    { 
+    
+    }
+
+    private void Die()
+    { 
+    
+    }
+}
