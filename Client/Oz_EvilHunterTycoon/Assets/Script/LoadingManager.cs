afde312678b276999597eee060a41daa1a106899
graypaun25 <rlawlsrn3262@gmail.com>
2026-04-10 17:21:44 +0900

    매니저 다음씬에 넘기고 각종오류 해결 및 확장

--- Client/Oz_EvilHunterTycoon/Assets/Script/LoadingManager.cs
+++ Client/Oz_EvilHunterTycoon/Assets/Script/LoadingManager.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadingManager : MonoBehaviour
+/// <summary>
+/// [팀원용] LoadingManager - BaseManager_KJG 상속 버전
+/// 이제 Manager_KJG.Loading으로 접근 가능 + DontDestroyOnLoad 자동 적용
+/// </summary>
+public class LoadingManager : BaseManager_KJG<LoadingManager>
 {
-    static string _nextScene;
+    private static string _nextScene;
+
+    protected override void Start()
+    {
+        base.Start();   // BaseManager_KJG 초기화 (DontDestroyOnLoad 등)
+        Debug.Log("[LoadingManager] 초기화 완료");
+    }
 
     public static void LoadScene(string sceneName)
     {
